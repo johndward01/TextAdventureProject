0d4d973882e82d4a11f42dbf25c65107a492cb15
johnd <johndward01@yahoo.com>
2020-08-12 16:22:45 -0500

    commit on 8/12/2020 (making sure all my projects have fresh commits on GitHub)

--- TextAdventure/DeadBody.cs
+++ TextAdventure/DeadBody.cs
@@ -6,11 +6,11 @@ namespace TextAdventure
 {
     public class DeadBody
     {
-        public int MoneyInWallet { get; set; }
+        public int MoneyInWallet { get; set; } = 0;
         public string DeadBodyDescription { get; set; } = "There's blood everywhere";
         public string PocketWatch { get; set; } = "The only thing in the man's pockets is an old silver pocketwatch.\nOdd..the pocketwatch's time is stopped at 6:30. 30 minutes after I left the office.";
         public string Shoes { get; set; } = "The shoes have mud on them";
-        public string Clothes { get; set; } = ""
+        public string Clothes { get; set; } = "On the inner stitching of his jacket there's an embroidery that says: Hitchcock";
 
         public void SearchBody()
         {
@@ -22,7 +22,8 @@ namespace TextAdventure
         }
         public void SearchClothes()
         {
-
+            Console.WriteLine(this.Shoes);
+            Console.WriteLine(this.Clothes);
         }
     }
 }
--- TextAdventure/MansionRooms/Bedroom.cs
+++ TextAdventure/MansionRooms/Bedroom.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace TextAdventure.MansionRooms
 {
-    class Bedroom
+    public class Bedroom : DeadBody
     {
+        public string BlankWalls { get; set; } = "An ordinary wallpaper design";
+        public string Bed { get; set; } = "An ordinary twin bed";
+        public string BedroomDoor { get; set; } = "The door is slightly ajar. (Was someone in here?)";
+        public string TaxodermyBird { get; set; } = "Looks like a stuffed crow..";
+        public bool IsInBedroom { get; set; }
+        public DeadBody MurderScene { get; set; }
+        public void EnterBedroom()
+        {
+            Console.WriteLine("Do you want to enter the bedroom? yes or no");
+            var userResponse = Console.ReadLine();
+            if (userResponse.ToLower() == "yes")
+            {
+                IsInBedroom = true;
+            }
+        }
+        public void SearchBedroom()
+        {
+            Console.WriteLine(this.Bed);
+            Console.WriteLine(this.TaxodermyBird);
+            Console.WriteLine($"");
+        }
+        public void ExitBedroom()
+        {
+            Console.WriteLine("Do you want to exit the bedroom? yes or no");
+            var userResponse = Console.ReadLine();
+            if (userResponse.ToLower() == "yes")
+            {
+                IsInBedroom = false;
+            }
+        }
+
     }
 }
